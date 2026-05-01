723b956560ca697dfb6166d1c1c3230bfe680274
LastRitter <lastritter@gmail.com>
2026-05-01 19:26:33 +0800

    switch repo to https proto.
    Change-Id: If2b2c9bc815066f67d10f01bdac298fe2ae4cd3c

--- Network.mk
+++ Network.mk
@@ -6,11 +6,38 @@
 
 
 REPO_URL_IP					?= 127.0.0.1
-REPO_URL_PROTO				?= ssh
+REPO_URL_PROTO				?= https
 REPO_URL_BASE				?= www.github.com
-REPO_URL_PORT				?= 22
+REPO_URL_PORT				?= 80
 REPO_URL_GROUP				?= kyberlab
+
+
+ifeq ($(REPO_URL_PROTO),ssh)
+
+ifeq ($(REPO_URL_PORT),22)
+REPO_URL_GIT_BASE			?= $(REPO_URL_PROTO)://git@$(REPO_URL_BASE)/$(REPO_URL_GROUP)
+else # ($(REPO_URL_PORT),22)
 REPO_URL_GIT_BASE			?= $(REPO_URL_PROTO)://git@$(REPO_URL_BASE):$(REPO_URL_PORT)/$(REPO_URL_GROUP)
+endif # ($(REPO_URL_PORT),22)
+
+else # ($(REPO_URL_PROTO),ssh)
+
+ifeq ($(REPO_URL_PROTO),https)
+
+ifeq ($(REPO_URL_PORT),80)
+REPO_URL_GIT_BASE			?= $(REPO_URL_PROTO)://$(REPO_URL_BASE)/$(REPO_URL_GROUP)
+else # ($(REPO_URL_PORT),80)
+REPO_URL_GIT_BASE			?= $(REPO_URL_PROTO)://$(REPO_URL_BASE)/$(REPO_URL_PORT)/$(REPO_URL_GROUP)
+endif # ($(REPO_URL_PORT),80)
+
+else # ($(REPO_URL_PROTO),https)
+
+$(error Unsupport REPO_URL_PROTO="$(REPO_URL_PROTO)")
+
+endif # ($(REPO_URL_PROTO),https)
+
+endif # ($(REPO_URL_PROTO),ssh)
+
 
 BENCH_REPO_BASE				?= $(REPO_URL_BASE)/docker
 
